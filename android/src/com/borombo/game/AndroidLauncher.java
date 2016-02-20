b87590e1882a955090a683d1b6c8ec70f1643bb9
Borombo <e.rombo@yahoo.com>
2016-02-20 02:53:40 +0100

    Initial Commit - App Structure

---
+++ android/src/com/borombo/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.borombo.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.borombo.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ core/src/com/borombo/game/MyGdxGame.java
@@ -0,0 +1,33 @@
+package com.borombo.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.borombo.game.States.GameStateManager;
+
+public class MyGdxGame extends ApplicationAdapter {
+	public final static int WHIDTH = 480;
+	public final static int HEIGHT = 800;
+
+	public final static String TITLE = "GDX Demo";
+
+	private GameStateManager gsm;
+	private SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
+	}
+}
